6dc0172516a47a283a56ae7e4fdb312c2b29f4fd
wildados <54842436+wildados@users.noreply.github.com>
2025-07-21 14:23:51 +0900

    Update server.py

--- server.py
+++ server.py
@@ -45,5 +45,5 @@ def generate():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
